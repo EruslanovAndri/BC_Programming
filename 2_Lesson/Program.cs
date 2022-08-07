2bd8eaf09c32cc1753c4bb7eef75ad578a4c2c89
EruslanovAndrei <eruslanov84as@gmail.com>
2022-08-07 13:14:22 +0300

    Lesson 2 O Notation

---
+++ 2_Lesson/Program.cs
@@ -0,0 +1,38 @@
+﻿// int n = 5;
+// int[] array = new int[n];
+// for (int i = 0; i < n; i++)
+// {
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// Console.WriteLine("[" + string.Join(", ", array) + "] ");
+
+// // Сложность алгоритма О(1)
+// int summa = 0;
+// for (int i = 0; i < n; i++)
+// {
+//     summa += array[i];
+
+// }
+// Console.WriteLine(summa);
+
+// O(n^2) время работы.
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j]);
+        Console.Write("\t");
+    }
+    Console.WriteLine();
+}
