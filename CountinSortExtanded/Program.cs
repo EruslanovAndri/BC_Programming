7b62920ceca41b3c39d2330591f14142e625af56
EruslanovAndrei <eruslanov84as@gmail.com>
2022-08-20 15:53:29 +0300

    Added Counting Sort Extanded

---
+++ CountinSortExtanded/Program.cs
@@ -0,0 +1,33 @@
+﻿// Работает только с цифрами
+//  Сортировка подсчетом.
+
+Console.Clear();
+int[] array = { -10, -5, 0, 4, 2, 10, 20, 5, 30 };
+
+int[] sortedArray = CountingSortExtanded(array);
+Console.WriteLine(string.Join(", ", sortedArray));
+
+int[] CountingSortExtanded(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+    int offset = -min;
+
+    int[] sortedArray = new int[inputArray.Length];
+    int[] counters = new int[max + offset + 1];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i] + offset]++;
+    }
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            sortedArray[index] = i - offset;
+            index++;
+        }
+    }
+    return sortedArray;
+}
